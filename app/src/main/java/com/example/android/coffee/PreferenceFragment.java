50972f7ffe734e6241d4058980dac65e5db470e1
JAPM <cicelcup@gmail.com>
2019-11-03 06:59:36 -0600

    add item decoration to recycler view

--- app/src/main/java/com/example/android/coffee/PreferenceFragment.java
+++ app/src/main/java/com/example/android/coffee/PreferenceFragment.java
@@ -1,13 +1,27 @@
 package com.example.android.coffee;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 public class PreferenceFragment extends PreferenceFragmentCompat
         implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(),
+                DividerItemDecoration.VERTICAL));
+        super.onViewCreated(view, savedInstanceState);
+    }
+
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.settings_main);
